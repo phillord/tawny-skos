c6736e91d5856ccac56481ed6d011ec4822c0fa3
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-03-05 15:49:34 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject tawny-skos "0.1"
+  :description "The SKOS vocabulary supports knowledge organisation systems, 
+vocabularies and thesauri. SKOS is documentated at
+http://www.w3.org/2004/02/skos/. This project imports SKOS into the 
+Tawny-OWL library."
+
+  :dependencies [[uk.org.russet/tawny-owl "0.10-SNAPSHOT"]
+                 [org.clojure/clojure "1.5.0"]])
